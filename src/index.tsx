177b9fd709bf60677cd881fc4ceb56dbd38c0b0d
Shiv <shiv7727@gmail.com>
2022-12-31 17:55:26 +0530

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import UserSearch from './refs/userSearch';
 
-const users = [
-    { name: "Sarah", age: 20 },
-    { name: "Alex", age: 20 },
-    { name: "Michael", age: 20 }
-]
+
 
 const App: React.FC = () => {
     return (
